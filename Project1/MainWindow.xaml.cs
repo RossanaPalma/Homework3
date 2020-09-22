7608bbaf511ee4c1d472872c65b3690600ce1c63
RossanaPalma <70126235+RossanaPalma@users.noreply.github.com>
2020-09-21 19:57:05 -0700

    Add project files.

---
+++ Project1/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿/// Chapter No. 3		Exercise No. 1
+/// File Name: Program.cs - Project 1
+/// /// @author: Rossana Palma
+/// Date:  September 21, 2020
+///
+/// Problem Statement: Write a GUI program that reads amount mortgage and calculate the monthly principal, monthly interest, monthly mortgage and the outstandings balance. Assume
+/// that the annual interest is 6.39 percent. Display the results on the screen.
+/// 
+/// Overall Plan:
+/// 1) Define variables.
+/// 2) Convert text to double.
+/// 3) Calculations.
+/// 4) Display the resuls on the screen.
+/// 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Define variables
+            double mortgageAmount, outstandingsBalance;
+            double annualInterest, monthlyMortgage, monthlyInterest, monthlyPrincipal;
+
+            // Convert text to Double
+            mortgageAmount = Convert.ToDouble(mortAmount.Text);
+            annualInterest = Convert.ToDouble(annualInt.Text);
+
+            // Calculation
+            monthlyInterest = mortgageAmount * ((annualInterest / 100) / 12);
+            monthlyPrincipal = mortgageAmount / 12;
+            monthlyMortgage = monthlyPrincipal + monthlyInterest;
+            outstandingsBalance = mortgageAmount - monthlyMortgage;
+
+            // Display output
+            outBalance.Text = outstandingsBalance.ToString("c");
+            annualInt.Text = annualInterest.ToString("c");
+            monthMort.Text = monthlyMortgage.ToString("c");
+            monthInt.Text = monthlyInterest.ToString("c");
+            monthPrinc.Text = monthlyPrincipal.ToString("c");
+                        
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Project2/Program.cs
@@ -0,0 +1,51 @@
+﻿/// Chapter No. 3		Exercise No. 2
+/// File Name: Program.cs - Project 2 
+/// /// @author: Rossana Palma
+/// Date:  September 21, 2020
+///
+/// Problem Statement: Write a program that determines the change to be dispensed by vending machine. Each item in the machine can be cost between
+/// 25 cents and 1 dollar, in 5-cent increments and the machine accepts only a single dollar bill to pay for the item.  
+/// 
+/// Overall Plan:
+/// 1) Declare variables.
+/// 2) Ask for price.
+/// 3) Calculates the change.
+/// 4) Display the purchase value and how the change will be given.
+/// 
+/// 
+using System;
+using System.Transactions;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            int n1, n2, n3, n4, amount; 
+
+            // Ask for price
+            Console.WriteLine("Enter price of item (from 25 cents to a dollar, in 5-cent increments)");
+            String userInput;
+            userInput = Console.ReadLine();
+            n1 = Int32.Parse(userInput);
+            
+            // Calculations
+            amount = 100 - n1; 
+            n2 = amount / 25;
+            amount = amount % 25;
+            n3 = amount / 10;
+            n4 = amount / 5;
+
+            // Display output
+
+            Console.WriteLine("You bought an item for " + n1 + " cents and gave me a dollar, so your change is ");
+            Console.WriteLine(n2 + " quarters");
+            Console.WriteLine(n3 + " dimes", "and");
+            Console.WriteLine(n4 + " nickles");
+           
+        }
+
+    }
+}
